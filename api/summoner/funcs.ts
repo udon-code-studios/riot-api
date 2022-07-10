2ac2a523a50292731b83a57d450ee09b7d16cb9d
leo6liu <leo.battalora@gmail.com>
2022-07-10 17:32:35 -0400

    require platform enum as host

--- api/summoner/funcs.ts
+++ api/summoner/funcs.ts
@@ -1,14 +1,17 @@
+import { Platform } from "../routes.ts";
 import { SummonerDTO } from "./types.ts";
 
 /** Get a summoner by summoner name. */
 export async function byName(
   name: string,
-  options: { host: string }
+  options: { region: Platform }
 ): Promise<{ status: number; summoner?: SummonerDTO }> {
-  const resp = await fetch(`https://${options.host}/lol/summoner/v4/summoners/by-name/${name}`, {
+  // make HTTP request
+  const resp = await fetch(`https://${options.region}/lol/summoner/v4/summoners/by-name/${name}`, {
     headers: { "X-Riot-Token": window.RIOT_API_KEY || "" },
   });
 
+  // extract JSON on 200 status
   let summoner: SummonerDTO | undefined = undefined;
   if (resp.status === 200) {
     summoner = await resp.json();
