db6fb13aa12a3e7fc9955e2db3ef4bf56b707848
leo6liu <leo.battalora@gmail.com>
2022-07-12 21:13:27 -0400

    make optional options optional

--- api/match/functs.ts
+++ api/match/functs.ts
@@ -6,17 +6,17 @@ export async function byPuuid(
   options: {
     region: Regional;
     /** Epoch timestamp in seconds. The matchlist started storing timestamps on June 16th, 2021. Any matches played before June 16th, 2021 won't be included in the results if the startTime filter is set. */
-    startTime: number;
+    startTime?: number;
     /** Epoch timestamp in seconds. */
-    endTime: number;
+    endTime?: number;
     /** Filter the list of match ids by a specific queue id. This filter is mutually inclusive of the type filter meaning any match ids returned must match both the queue and type filters. */
-    queue: number;
+    queue?: number;
     /** Filter the list of match ids by the type of match. This filter is mutually inclusive of the queue filter meaning any match ids returned must match both the queue and type filters. */
-    type: string;
+    type?: string;
     /** Defaults to 0. Start index. */
-    start: number;
+    start?: number;
     /** Defaults to 20. Valid values: 0 to 100. Number of match ids to return. */
-    count: number;
+    count?: number;
   }
 ): Promise<{ status: number; matches?: string[] }> {
   // make HTTP request
